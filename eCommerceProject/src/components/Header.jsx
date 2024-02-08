42f768d6e04b3b3c2163a9b8deb5624b7eeb703d
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-08 22:15:44 +0300

    fontawesome installation and import completed

--- eCommerceProject/src/components/Header.jsx
+++ eCommerceProject/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Header() {
   return (
-    <div>
+    <header>
+      <FontAwesomeIcon icon={faGithub} />
       <div className="part-1"></div>
       <div className="part-2"></div>
       <div className="part-3"></div>
-    </div>
+    </header>
   );
 }
