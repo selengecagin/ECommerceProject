8abeb8688f4f266c6d4a9332a0dad9d27ca18fd5
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-09 11:32:00 +0300

    footer folder created

--- eCommerceProject/src/pages/HomePage.jsx
+++ eCommerceProject/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function HomePage() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Footer />
     </div>
-  )
+  );
 }
