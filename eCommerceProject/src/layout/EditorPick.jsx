0332170c2f289eb321149754a7965e42e3142751
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-14 09:05:14 +0300

    product information updated

--- eCommerceProject/src/layout/EditorPick.jsx
+++ eCommerceProject/src/layout/EditorPick.jsx
@@ -1,29 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import editorsPick1 from "../assets/images/editorsPick1.png";
 import editorsPick2 from "../assets/images/editorsPick2.png";
 import editorsPick3 from "../assets/images/editorsPick3.png";
 import editorsPick4 from "../assets/images/editorsPick4.png";
-import { Link } from "react-router-dom";
+
 
 export default function EditorPick() {
   return (
-    <main className="flex flex-col px-48 py-20 gap-12 bg-[#FAFAFA] items-center justify-center ">
-      <div className="flex flex-col items-center gap-2.5 ">
-        <h2 className="text-2xl font-bold text-[#252B42]">EDITOR’S PICK</h2>
-        <p className="text-sm font-normal text-[#737373]">
-          Problems trying to resolve the conflict between{" "}
+    <main className="flex flex-col  py-20 gap-12 bg-[#FAFAFA] items-center justify-center ">
+      <section className="flex flex-col items-center gap-2.5 ">
+        <h2 className="text-2xl font-bold text-darkTextColor">EDITOR’S PICK</h2>
+        <p className="text-sm font-normal text-secondTextColor">
+          Problems trying to resolve the conflict between
         </p>
-      </div>
-      <div className="flex gap-8 justify-center">
+      </section>
+
+      <section className="flex gap-8 justify-center">
         <div className="relative">
           <img
             src={editorsPick1}
-            alt="editors-pick-image-1"
+            alt="Editors pick for mens"
             className="w-full"
           />
-          <div className="absolute w-[170px] h-12 inset-0 flex items-center justify-center bg-white">
+          <div className="absolute bottom-0 transform -translate-x-1/2  w-[170px] h-12 inset-0 flex items-center justify-center bg-white">
             <div className="text-center">
-              <Link to="/men" className="text-[#252B42] font-bold ">
+              <Link to="/men" className="text-darkTextColor font-bold ">
                 MEN
               </Link>
             </div>
@@ -31,10 +33,13 @@ export default function EditorPick() {
         </div>
 
         <div className="relative">
-          <img src={editorsPick2} alt="editors-pick-img-2" />
+          <img src={editorsPick2} alt="Editors pick for women" />
           <div className="absolute flex items-center justify-center bg-white w-[136px]">
             <div className="text-center">
-              <Link to="/women" className="text-[#252B42] font-bold text-base">
+              <Link
+                to="/women"
+                className="text-darkTextColor font-bold text-base"
+              >
                 WOMEN
               </Link>
             </div>
@@ -45,12 +50,15 @@ export default function EditorPick() {
           <div className="relative">
             <img
               src={editorsPick3}
-              alt="editors-pick-img-"
+              alt="Editors pick for accessories"
               className="w-full"
             />
             <div className=" absolute w-[170px] h-12 inset-0 flex items-center justify-center bg-white">
               <div className="text-center">
-                <Link to="/accessories" className="text-[#252B42] font-bold ">
+                <Link
+                  to="/accessories"
+                  className="text-darkTextColor font-bold "
+                >
                   ACCESSORIES
                 </Link>
               </div>
@@ -58,18 +66,21 @@ export default function EditorPick() {
           </div>
 
           <div className="relative">
-            <img src={editorsPick4} alt="Image 4" className="w-full" />
+            <img
+              src={editorsPick4}
+              alt="Editors pick for kids"
+              className="w-full"
+            />
             <div className=" absolute  w-[170px] h-12 inset-0 flex items-center justify-center bg-white">
               <div className="text-center">
-                <Link to="/kids" className="text-[#252B42] font-bold ">
+                <Link to="/kids" className="text-darkTextColor font-bold ">
                   KIDS
                 </Link>
               </div>
             </div>
-
           </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
--- eCommerceProject/src/layout/ProductInformation.jsx
+++ eCommerceProject/src/layout/ProductInformation.jsx
@@ -6,33 +6,40 @@ import readMore from "../assets/images/readMoreButton.svg";
 export default function ProductInformation() {
   return (
     <main className="flex flex-row justify-center items-center gap-8 px-48">
-      <div className="flex justify-center">
+      <section className="flex justify-center">
         <img
           src={pInfo}
           alt="Product image description"
           className="flex-shrink-0"
-        ></img>
-      </div>
-      <div className="flex flex-col gap-8">
-        <h3 className="text-base font-bold text-[#BDBDBD]">SUMMER 2020</h3>
-        <h1 className="text-4xl font-bold text-[#252B42]">
+        />
+      </section>
+
+      <section className="flex flex-col gap-8">
+        <h3 className="text-base font-bold text-mutedColor">SUMMER 2020</h3>
+        <h1 className="text-4xl font-bold text-darkTextColor">
           Part of the Neural Universe
         </h1>
-        <p className="flex flex-wrap text-xl font-normal text-[#737373]">
+        <p className="flex flex-wrap text-xl font-normal text-secondTextColor">
           We know how large objects will act,
           <br />
           but things on a small scale.
         </p>
 
         <div className="flex flex-row gap-2.5 ">
-          <button className="hover:scale-110 hover:drop-shadow-lg transition duration-300">
-            <img src={buyNow} alt="Buy now"></img>
+          <button
+            className="hover:scale-110 hover:drop-shadow-lg transition duration-300"
+            aria-label="Buy now"
+          >
+            <img src={buyNow} alt="Click here to buy" />
           </button>
-          <button className="hover:scale-110 hover:drop-shadow-lg transition duration-300">
-            <img src={readMore} alt="Read more"></img>
+          <button
+            className="hover:scale-110 hover:drop-shadow-lg transition duration-300"
+            aria-label="Read more"
+          >
+            <img src={readMore} alt="Read more" />
           </button>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
