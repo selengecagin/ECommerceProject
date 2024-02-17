547e39652953122fe735dc312ef502cf15af8788
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-17 10:27:05 +0300

    route logic moved to app.jsx

--- eCommerceProject/src/App.jsx
+++ eCommerceProject/src/App.jsx
@@ -1,10 +1,34 @@
 import "./App.css";
-import PageContent from "./layout/PageContent";
 
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+import AboutPage from "./pages/AboutPage";
+import TeamPage from "./pages/TeamPage";
+import ContactPage from "./pages/ContactPage";
+import ProductPage from "./pages/ProductPage";
 function App() {
   return (
     <>
-      <PageContent />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+
+        <Route path="/about-page" element={<AboutPage />}></Route>
+
+        <Route path="/product-list-page" element={<ProductListPage />}></Route>
+
+        <Route path="/team-page" element={<TeamPage />}></Route>
+
+        <Route path="/contact-page" element={<ContactPage />}></Route>
+
+        <Route path="/pages" element={<ProductPage />}></Route>
+
+        <Route path="/sign-in" element={<HomePage />}></Route>
+
+        <Route path="/sign-up" element={<HomePage />}></Route>
+
+        <Route path="*" element={<h1>404 Not Found Page</h1>} />
+      </Routes>
     </>
   );
 }
