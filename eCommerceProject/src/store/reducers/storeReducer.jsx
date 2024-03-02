3f4d2165ba3f8d3498275f6ecba66656e90bba08
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-03-02 14:58:23 +0300

    store reducer updated

--- eCommerceProject/src/store/reducers/storeReducer.jsx
+++ eCommerceProject/src/store/reducers/storeReducer.jsx
@@ -2,23 +2,11 @@ const initialState = {
   store: {},
 };
 
-const SET_STORE = "SET_STORE";
-
 const storeReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_STORE:
-      return {
-        ...state,
-        store: action.payload,
-      };
     default:
       return state;
   }
 };
-
-export const setStore = (store) => ({
-    type: SET_STORE,
-    payload: store
-});
 
 export default storeReducer;
